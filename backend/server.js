a8f30e106855cd97807bab30bfa59a075965bbdb
MaseCodes20 <95643884+MaseCodes20@users.noreply.github.com>
2022-11-14 11:38:00 -0500

    connect to Mongoose database

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,22 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
+const { errorHandler } = require("./middlewares/errorMiddleware");
+const connectDB = require("./config/db");
 
 const port = process.env.PORT || 8000;
 
+// Connect to Mongoose Database
+connectDB();
+
 const app = express();
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(errorHandler);
+
+// Routes
 
 app.listen(port, () => console.log(`server is running on port ${port}`));
