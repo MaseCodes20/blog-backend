60da5177234f736a94d72e66dcffa18fbf918a82
MaseCodes20 <95643884+MaseCodes20@users.noreply.github.com>
2022-11-15 10:58:00 -0500

    Auth middleware

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,61 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+
+const User = require("../models/userModel");
+
+const verifyToken = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      // Get token from header
+      token = req.headers.authorization.split(" ")[1];
+
+      // Verify Token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      // Get user from the token
+      req.user = await User.findById(decoded.id).select("-password");
+
+      next();
+    } catch (error) {
+      console.log(error);
+      res.status(401);
+      throw new Error("Not authorized");
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+});
+
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user?.id === req.params?.userID || req.user?.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not allowed to do that!");
+    }
+  });
+};
+
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user?.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("Admin access only!");
+    }
+  });
+};
+
+module.exports = {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+};
