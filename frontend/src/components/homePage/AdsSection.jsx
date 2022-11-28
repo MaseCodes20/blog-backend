a429fdb0e7859865b525172505d8b7ff15e59ee8
MaseCodes20 <95643884+MaseCodes20@users.noreply.github.com>
2022-11-28 18:07:14 -0500

    home layot created

---
+++ frontend/src/components/homePage/AdsSection.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+const Ads = [
+  {
+    url: "",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6a6N6vyk_I8goH0KsflNyLC6dZUqqUuylrA&usqp=CAU",
+    companyName: "Advertisment",
+  },
+  {
+    url: "",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6a6N6vyk_I8goH0KsflNyLC6dZUqqUuylrA&usqp=CAU",
+    companyName: "Advertisment",
+  },
+  {
+    url: "",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6a6N6vyk_I8goH0KsflNyLC6dZUqqUuylrA&usqp=CAU",
+    companyName: "Advertisment",
+  },
+  {
+    url: "",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6a6N6vyk_I8goH0KsflNyLC6dZUqqUuylrA&usqp=CAU",
+    companyName: "Advertisment",
+  },
+];
+
+function AdsSection() {
+  return (
+    <div className="p-5 flex-[2]">
+      <div className="grid grid-cols-1 gap-10">
+        {Ads.map((ad) => {
+          return (
+            <button className="relative">
+              <img
+                src={ad.image}
+                alt=""
+                className="h-[100px] w-[300px] object-cover"
+              />
+              <div className="absolute top-0 right-0 bottom-0 left-0 flex items-center justify-center">
+                <p className="font-bold bg-white p-2">{ad.companyName}</p>
+              </div>
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default AdsSection;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import AdsSection from "../components/homePage/AdsSection";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="pageContainer flex">
+      {/* SideBar menu */}
+      <div className="h-[calc(100vh-75px)] flex-[2] sticky top-[75px] border-r-[1px] border-black">
+        <Link to="/">Home</Link>
+
+        <div>
+          <h3>Tags</h3>
+
+          <button>Javascript</button>
+        </div>
+
+        <button>popular</button>
+      </div>
+
+      {/* Posts */}
+      <div className="bg-green-400 h-screen p-5 flex-[6]">Feed</div>
+
+      {/* Ads */}
+      <AdsSection />
+    </div>
+  );
 }
 
 export default Home;
