06cbe3aa89eba6c4f273744b2a96684365c42a08
MaseCodes20 <95643884+MaseCodes20@users.noreply.github.com>
2022-12-13 21:56:17 -0500

    added logEvents to errorHandler

--- backend/middlewares/errorMiddleware.js
+++ backend/middlewares/errorMiddleware.js
@@ -1,4 +1,11 @@
+const { logEvents } = require("./logger");
+
 const errorHandler = (err, req, res, next) => {
+  logEvents(
+    `${err.name}: ${err.message}\t${req.method}\t${req.url}\t${req.headers.origin}`,
+    "errLog.log"
+  );
+
   const statusCode = res.statusCode ? res.statusCode : 500;
 
   res.status(statusCode);
