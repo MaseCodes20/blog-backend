8b8a963a9d3b7d593223dc0e746777e7bba310aa
MaseCodes20 <95643884+MaseCodes20@users.noreply.github.com>
2022-12-13 20:43:45 -0500

    Routing: root route display html and routing bad requests

---
+++ backend/config/allowedOrigins.js
@@ -0,0 +1,3 @@
+const allowedorigins = ["http://localhost:3000"];
+
+module.exports = allowedorigins;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config();
+const path = require("path");
 const cors = require("cors");
 const { errorHandler } = require("./middlewares/errorMiddleware");
 const connectDB = require("./config/db");
@@ -17,9 +18,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Routes
+app.use("/", express.static(path.join(__dirname, "/public")));
+app.use("/", require("./routes/root"));
+
 app.use("/api/v1/auth", require("./routes/authRoutes"));
 app.use("/api/v1/users", require("./routes/userRoutes"));
 app.use("/api/v1/posts", require("./routes/postRoutes"));
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 Not Fount" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
+});
 
 // ERROR HANDLER MIDDLEWARE (Last middleware to use)
 app.use(errorHandler);
