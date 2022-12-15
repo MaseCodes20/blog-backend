78cd8195014cd36161028db04016a631f54c7351
MaseCodes20 <95643884+MaseCodes20@users.noreply.github.com>
2022-12-15 13:19:24 -0500

    allowed origins update

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,4 +1,7 @@
 // const allowedorigins = ["http://localhost:3000"];
-const allowedorigins = ["https://mcblogwebsite.onrender.com"];
+const allowedorigins = [
+  "https://mcblogwebsite.onrender.com",
+  "https://mcblogwebsite-api.onrender.com",
+];
 
 module.exports = allowedorigins;
