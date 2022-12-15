b24184046f075d6225e7e61bfe230880fa352a41
MaseCodes20 <95643884+MaseCodes20@users.noreply.github.com>
2022-12-14 19:21:15 -0500

    Backend is ready to deploy

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,3 +1,4 @@
-const allowedorigins = ["http://localhost:3000"];
+// const allowedorigins = ["http://localhost:3000"];
+const allowedorigins = ["https://mcblogwebsite.onrender.com"];
 
 module.exports = allowedorigins;
